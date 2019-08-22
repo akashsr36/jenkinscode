b937f5c63f51792f92db9b61babc887e7bd20457
akashsr36 <54150421+akashsr36@users.noreply.github.com>
2019-08-22 13:07:28 -0400

    Update number.sh

--- number.sh
+++ number.sh
@@ -1,5 +1,5 @@
-echo "First number is $num1=5" 
-echo "Second number is $num2=12" 
+echo "First number is $num1" 
+echo "Second number is $num2" 
 echo "Addition of both the numbers is : $((num1 + num2))"
 echo "This script will now be pulled from webhook" 
 echo " New changes added" 
